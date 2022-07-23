2f478071e96a356b684d68e13b47cf32f674b3dc
Ian Qvist <qvistian@gmail.com>
2022-07-23 02:49:58 +0200

    Reactivate all benchmarks

--- src/FastHash.Benchmarks/HashBenchmarks.cs
+++ src/FastHash.Benchmarks/HashBenchmarks.cs
@@ -25,7 +25,7 @@ public class HashBenchmarks : IDisposable
     private unsafe byte* _ptr;
     private byte[] _testData = null!;
 
-    [Params(8)]
+    [Params(8, 32, 128, 1024, 1024 * 1024 * 32)]
     public int Size { get; set; }
 
     public unsafe void Dispose()
@@ -44,93 +44,92 @@ public class HashBenchmarks : IDisposable
             _ptr[i] = _testData[i];
     }
 
-    // [Benchmark]
-    // public uint DJBHash32Test() => DJB2Hash32.ComputeHash(_testData);
-    //
-    // [Benchmark]
-    // public uint FarmHash32Test() => FarmHash32.ComputeHash(_testData);
-    //
-    // [Benchmark]
-    // public ulong FarmHash64Test() => FarmHash64.ComputeHash(_testData);
-    //
-    // [Benchmark]
-    // public ulong FarshHash64Test() => FarshHash64.ComputeHash(_testData);
-    //
-    // [Benchmark]
-    // public uint FNV1a32Test() => FNV1a32.ComputeHash(_testData);
-    //
-    // [Benchmark]
-    // public ulong FNV1a64Test() => FNV1a64.ComputeHash(_testData);
-    //
-    // [Benchmark]
-    // public uint Marvin32Test() => Marvin32.ComputeHash(_testData, (uint)_testData.Length, 43);
-    //
-    // [Benchmark]
-    // public uint MurmurHash32Test() => Murmur3Hash32.ComputeHash(_testData);
-    //
-    // [Benchmark]
-    // public Uint128 MurmurHash128Test() => Murmur3Hash128.ComputeHash(_testData);
-    //
-    // [Benchmark]
-    // public ulong SipHash64Test() => SipHash64.ComputeHash(_testData);
-    //
-    // [Benchmark]
-    // public uint SuperFastHash32Test() => SuperFastHash32.ComputeHash(_testData);
-    //
-    // [Benchmark]
-    // public uint xx2Hash32Test() => xx2Hash32.ComputeHash(_testData);
-    //
-    // [Benchmark]
-    // public ulong xx2Hash64Test() => xx2Hash64.ComputeHash(_testData);
+    [Benchmark]
+    public uint Djb2Hash32Test() => Djb2Hash32.ComputeHash(_testData);
 
-    // [Benchmark]
-    // public ulong wy3Hash64Test() => wy3Hash64.ComputeHash(_testData);
+    [Benchmark]
+    public uint FarmHash32Test() => FarmHash32.ComputeHash(_testData);
 
-    //
-    // [Benchmark]
-    // public unsafe uint DJBHash32UnsafeTest() => DJB2Hash32Unsafe.ComputeHash(_ptr, _testData.Length);
-    //
-    // [Benchmark]
-    // public unsafe uint FarmHash32UnsafeTest() => FarmHash32Unsafe.ComputeHash(_ptr, _testData.Length);
-    //
-    // [Benchmark]
-    // public unsafe ulong FarshHash64UnsafeTest() => FarshHash64Unsafe.ComputeHash(_ptr, _testData.Length);
-    //
-    // [Benchmark]
-    // public unsafe uint FNV1a32UnsafeTest() => FNV1a32Unsafe.ComputeHash(_ptr, _testData.Length);
-    //
-    // [Benchmark]
-    // public unsafe ulong FNV1a64UnsafeTest() => FNV1a64Unsafe.ComputeHash(_ptr, _testData.Length);
-    //
-    // [Benchmark]
-    // public unsafe ulong FNV1aYT32UnsafeTest() => FNV1aYT32Unsafe.ComputeHash(_ptr, _testData.Length);
-    //
-    // [Benchmark]
-    // public unsafe uint MurmurHash32UnsafeTest() => Murmur3Hash32Unsafe.ComputeHash(_ptr, _testData.Length);
-    //
-    // [Benchmark]
-    // public unsafe Uint128 MurmurHash128UnsafeTest() => Murmur3Hash128Unsafe.ComputeHash(_ptr, _testData.Length);
-    //
-    // [Benchmark]
-    // public unsafe ulong SipHash64UnsafeTest() => SipHash64Unsafe.ComputeHash(_ptr, _testData.Length);
-    //
-    // [Benchmark]
-    // public unsafe uint SuperFastHash32UnsafeTest() => SuperFastHash32Unsafe.ComputeHash(_ptr, _testData.Length);
-    //
-    // [Benchmark]
-    // public unsafe uint xx2Hash32UnsafeTest() => xx2Hash32Unsafe.ComputeHash(_ptr, _testData.Length);
-    //
-    // [Benchmark]
-    // public unsafe ulong xx2Hash64UnsafeTest() => xx2Hash64Unsafe.ComputeHash(_ptr, _testData.Length);
-    //
-    // [Benchmark]
-    // public unsafe ulong xx3Hash64UnsafeTest() => xx3Hash64Unsafe.ComputeHash(_ptr, _testData.Length);
-    //
-    // [Benchmark]
-    // public unsafe Uint128 xx3Hash128UnsafeTest() => xx3Hash128Unsafe.ComputeHash(_ptr, _testData.Length);
-    //
-    // [Benchmark]
-    // public unsafe ulong wy3Hash64UnsafeTest() => wy3Hash64Unsafe.ComputeHash(_ptr, _testData.Length);
+    [Benchmark]
+    public ulong FarmHash64Test() => FarmHash64.ComputeHash(_testData);
+
+    [Benchmark]
+    public ulong FarshHash64Test() => FarshHash64.ComputeHash(_testData);
+
+    [Benchmark]
+    public uint Fnv1aHash32Test() => Fnv1aHash32.ComputeHash(_testData);
+
+    [Benchmark]
+    public ulong Fnv1aHash64Test() => Fnv1aHash64.ComputeHash(_testData);
+
+    [Benchmark]
+    public uint MarvinHash32Test() => MarvinHash32.ComputeHash(_testData, (uint)_testData.Length, 43);
+
+    [Benchmark]
+    public uint Murmur3Hash32Test() => Murmur3Hash32.ComputeHash(_testData);
+
+    [Benchmark]
+    public Uint128 Murmur3Hash128Test() => Murmur3Hash128.ComputeHash(_testData);
+
+    [Benchmark]
+    public ulong SipHash64Test() => SipHash64.ComputeHash(_testData);
+
+    [Benchmark]
+    public uint SuperFastHash32Test() => SuperFastHash32.ComputeHash(_testData);
+
+    [Benchmark]
+    public uint Xx2Hash32Test() => Xx2Hash32.ComputeHash(_testData);
+
+    [Benchmark]
+    public ulong Xx2Hash64Test() => Xx2Hash64.ComputeHash(_testData);
+
+    [Benchmark]
+    public ulong Wy3Hash64Test() => Wy3Hash64.ComputeHash(_testData);
+
+    [Benchmark]
+    public unsafe uint Djb2Hash32UnsafeTest() => Djb2Hash32Unsafe.ComputeHash(_ptr, _testData.Length);
+
+    [Benchmark]
+    public unsafe uint FarmHash32UnsafeTest() => FarmHash32Unsafe.ComputeHash(_ptr, _testData.Length);
+
+    [Benchmark]
+    public unsafe ulong FarshHash64UnsafeTest() => FarshHash64Unsafe.ComputeHash(_ptr, _testData.Length);
+
+    [Benchmark]
+    public unsafe uint Fnv1aHash32UnsafeTest() => Fnv1aHash32Unsafe.ComputeHash(_ptr, _testData.Length);
+
+    [Benchmark]
+    public unsafe ulong Fnv1aHash64UnsafeTest() => Fnv1aHash64Unsafe.ComputeHash(_ptr, _testData.Length);
+
+    [Benchmark]
+    public unsafe ulong Fnv1aYtHash32UnsafeTest() => Fnv1aYtHash32Unsafe.ComputeHash(_ptr, _testData.Length);
+
+    [Benchmark]
+    public unsafe uint Murmur3Hash32UnsafeTest() => Murmur3Hash32Unsafe.ComputeHash(_ptr, _testData.Length);
+
+    [Benchmark]
+    public unsafe Uint128 Murmur3Hash128UnsafeTest() => Murmur3Hash128Unsafe.ComputeHash(_ptr, _testData.Length);
+
+    [Benchmark]
+    public unsafe ulong SipHash64UnsafeTest() => SipHash64Unsafe.ComputeHash(_ptr, _testData.Length);
+
+    [Benchmark]
+    public unsafe uint SuperFastHash32UnsafeTest() => SuperFastHash32Unsafe.ComputeHash(_ptr, _testData.Length);
+
+    [Benchmark]
+    public unsafe uint Xx2Hash32UnsafeTest() => Xx2Hash32Unsafe.ComputeHash(_ptr, _testData.Length);
+
+    [Benchmark]
+    public unsafe ulong Xx2Hash64UnsafeTest() => Xx2Hash64Unsafe.ComputeHash(_ptr, _testData.Length);
+
+    [Benchmark]
+    public unsafe ulong Xx3Hash64UnsafeTest() => Xx3Hash64Unsafe.ComputeHash(_ptr, _testData.Length);
+
+    [Benchmark]
+    public unsafe Uint128 Xx3Hash128UnsafeTest() => Xx3Hash128Unsafe.ComputeHash(_ptr, _testData.Length);
+
+    [Benchmark]
+    public unsafe ulong Wy3Hash64UnsafeTest() => Wy3Hash64Unsafe.ComputeHash(_ptr, _testData.Length);
 
     private byte[] GetRandomBytes(int count)
     {
